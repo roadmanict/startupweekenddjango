d7ec5b2eec84e5697e9062b879692826224c6974
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2017-06-16 15:56:39 +0200

    Rename startupweekend to startup

--- startup/settings/base.py
+++ startup/settings/base.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'startupweekend.urls'
+ROOT_URLCONF = 'startup.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'startupweekend.wsgi.application'
+WSGI_APPLICATION = 'startup.wsgi.application'
 
 
 # Database
