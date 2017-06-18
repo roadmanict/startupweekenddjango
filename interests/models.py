2b9ac8c962619539232722fe3980250f531aca01
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2017-06-18 12:10:34 +0200

    Return str for Interest model

--- interests/models.py
+++ interests/models.py
@@ -5,3 +5,6 @@ class Interest(models.Model):
     email = models.EmailField()
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.email
